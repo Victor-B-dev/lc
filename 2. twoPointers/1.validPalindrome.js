5d22ef1a5edff82859032fbfae66990371c33476
Victor <ban.victor728@gmail.com>
2024-10-28 13:52:40 -0400

    added validPalindrome reverse string solution  with comments

--- 2. twoPointers/1.validPalindrome.js
+++ 2. twoPointers/1.validPalindrome.js
@@ -31,22 +31,49 @@ Constraints:
 
 */
 
-// Reversing the string
+// Reversing the string - ASCII conversion
 
-/* We can make a new string that iterates through the string and reverses it.
-We still need to check for the alphanumeric.
+var validPalindrome = function (s) {
+  let sanitizedStr = "";
+  
+  // return a true or false based on alphanumeric
+  const isAlphanumeric = (char) => {
+    return ((char >= 'a' && char <= 'z') || 
+    (char >= 'A' && char <= 'Z') || 
+    (char >= '0' && char <= '9'))
+  }
 
-However this takes up additional space to store the string, so enter the two pointer strat for a solution that takes less space complexity.
+  // sanitize the original string by checking every string if its alphanumeric, if it is, add it to the revers
+  for (let char of s){
+    if (this.isAlphanumeric(char)){
+      sanitizedStr =+ char.toLowerCase();
+    }
+  }
 
+  // compare the sanitized string with its reversed version
+  return sanitizedStr === sanitizedStr.split("").reverse().join("");
+}
+
+/* The hint to sanitize is provided by the constraint & also related back to the groupAnagrams problem in arrayshashing.
+
+We can check the value of a character to see if it is in the contiguous ranges of alphanumeric characters (i.e. we know the ascii values of alphanumeric characters fall in 3 ranges (lowercase, uppercase, numeric), if it's not in any of those ranges, it's not alphanumeric)
+
+We rewite it as a helper function to be called per letter.
+
+As usual with JS, it's a bit limited with string methods so we need to convert it into a array, reverse it, join it back together into a string to do the reverse procedure.
+
+However this solution takes up additional space to store the new string, so enter the two pointer strat for a solution that takes less space complexity.
 */
 
-// Two Pointers - ASCII conversion
-/* This hint is provided by the constraint & also related back to the groupAnagrams problem in arrayshashing.
-We can check the value of a character to see if it is in the continguous ranges of alphanumeric characters (i.e. we know the ascii values of alphanumeric characters fall in 3 ranges, if it's not in any of those ranges, it's not alphanumeric)
+// Two Pointers 
+/* 
 
 */
 
 // Two Pointers - Regex
-/* We can also use regex to check for alphanumeric characters, not provided by the constraint, just some food for thought.
+
+const stringOnly = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+
+/* We can also use regex to check for alphanumeric characters, not provided by the constraint, just some food for thought due to the power of regex commands.
 
 */
