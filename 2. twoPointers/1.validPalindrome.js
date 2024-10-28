0c6a32c6df04c376f19d0a3008a81fc6803788c2
Victor <ban.victor728@gmail.com>
2024-10-28 14:20:14 -0400

    added twopointer solution with regex

--- 2. twoPointers/1.validPalindrome.js
+++ 2. twoPointers/1.validPalindrome.js
@@ -31,7 +31,7 @@ Constraints:
 
 */
 
-// Reversing the string - ASCII conversion
+// 1. Reversing the string - ASCII conversion
 
 var validPalindromeReverse = function (s) {
   let sanitizedStr = "";
@@ -65,36 +65,68 @@ As usual with JS, it's a bit limited with string methods so we need to convert i
 However this solution takes up additional space to store the new string, so enter the two pointer strat for a solution that takes less space complexity.
 */
 
-// Two Pointers
+// 2.1 Two Pointers
 var validPalindromeTwoPointer = function (s) {
 
-  // return a true or false based on alphanumeric
-  const isAlphanumeric = (char) => {
+  const isAlphanumeric = (char) => {  // still need to implement Alphanumeric check
     return ((char >= 'a' && char <= 'z') || 
     (char >= 'A' && char <= 'Z') || 
     (char >= '0' && char <= '9'))
   }
 
-  // sanitize the original string by checking every string if its alphanumeric, if it is, add it to the revers
-  for (let char of s){
-    if (this.isAlphanumeric(char)){
-      sanitizedStr =+ char.toLowerCase();
+  let left = 0; right = s.length - 1; // create pointers (variables) - one left, one right (array is length -1) 
+
+  while (left < right ) { // using a while loop - we iterate until the pointers meet in the middle
+    while (left < right && !isAlphanumeric(s[left])) { // using a while loop, we check if a character is an alphanumeric, if it isn't we go to the next letter. left pointer then sits there while the next loop runs
+      left++;
     }
+    while (right > left && !isAlphanumeric(s[right])) { // similar, we do the check from the right side, if it's not alphanumeric, skip it until it is, right pointer is now on an alphanum char
+      right--;
+    }
+    if (s[left].toLowerCase() !== s[right].toLowerCase()){ // we check if the lowercase version of the letters match at each pointer
+      return false; // escape early if they aren't
+    }
+    left++; // increment to next letter
+    right--; // decrement to next letter
   }
 
-  // compare the sanitized string with its reversed version
-  return sanitizedStr === sanitizedStr.split("").reverse().join("");
+  return true // if the whole string is passed through with no problem, it means it's a valid palindrome
 }
 
 
-/* 
+/* Two Pointer is an integral concept to learn to iterate through something in one pass without needing additional space.
+Versatile usages will be in future problems.
 
 */
 
-// Two Pointers - Regex
+// 2.2 Two Pointers - Regex
+var validPalindromeTwoPointerRegex = function (s) {
 
-const stringOnly = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+  const isAlphanumeric = (char) => {
+    return /^[a-zA-Z0-9]$/.test(char);
+  }
 
-/* We can also use regex to check for alphanumeric characters, not provided by the constraint, just some food for thought due to the power of regex commands.
+  let left = 0; right = s.length - 1;
 
+  while (left < right ) {
+    while (left < right && !isAlphanumeric(s[left])) {
+      left++;
+    }
+    while (right > left && !isAlphanumeric(s[right])) {
+      right--;
+    }
+    if (s[left].toLowerCase() !== s[right].toLowerCase()){
+      return false;
+    }
+    left++; right--;
+  }
+
+  return true
+}
+
+/* We can also use regex to check for alphanumeric characters, not provided by the constraint, just some food for thought due to the power of regex commands in manipulating/sanitizing strings. 
+It's concise & in my opinion, easier to read once you learn.
+
+Regex is contained in the //
+What the command is saying = test if the character begins (^) with characters in any of these ranges [a-z] [A-Z] [0-9]
 */
