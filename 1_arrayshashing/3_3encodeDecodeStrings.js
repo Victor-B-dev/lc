b25dfaf998ac07b0c251bb4e856a893cdafeac03
Victor <ban.victor728@gmail.com>
2024-10-29 20:55:03 -0400

    added topK & encode problems

---
+++ 1_arrayshashing/3_3encodeDecodeStrings.js
@@ -0,0 +1,27 @@
+// Believe this is leet code # 271
+/* String Encode and Decode
+
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement "encode" and decode"
+
+Example 1:
+
+Input: ["learning","is","very","hard"]["learning","is","very","hard"]
+
+Output: ["learning","is","very","hard"]
+
+Example 2:
+
+Input: ["we","say",":","yes"]
+Output: ["we","say",":","yes"]
+
+Constraints:
+
+0 <= strs.length < 100
+0 <= strs[i].length < 200
+strs[i] contains only UTF-8 characters.
+*/
+
+// Solution - Encoding is the easy part, Decoding is the hard part
+    
