536d7e55f1797b042a5893be1f8f1a1030403d68
Victor <ban.victor728@gmail.com>
2024-10-30 16:34:13 -0400

    added encode & decode solution

--- 1_arrayshashing/3_3encodeDecodeStrings.js
+++ 1_arrayshashing/3_3encodeDecodeStrings.js
@@ -24,4 +24,57 @@ strs[i] contains only UTF-8 characters.
 */
 
 // Solution - Encoding is the easy part, Decoding is the hard part
+
+const encode = (strs) => {
+  let result = '';
+  for (let s of strs) {
+      result += `${s.length}#${s}`;
+  }
+  return result;
+}
+
+const decode = (str) => {
+  let result = [];
+  let i = 0;
+
+  while (i < str.length) {
+      let j = i;
+      while (str[j] !== '#') {
+          j++;
+      }
+      let length = parseInt(str.substring(i, j), 10);
+      i = j + 1;
+      j = i + length;
+      result.push(str.substring(i, j));
+      i = j;
+  }
+  return result;
+}
+
+/* Encoding is decepctively simple in that we need to add character values between the strings to separate them.
+The problem however is if that special char is if that is actually included in one of the given strings.
+
+How we get around this is by adding another value. In this case, we are adding length to help us count the number of characters after the # sign (also known as a delimiter in this case).
+
+We make use of two pointers to find the limiter (#) and a substring goes from # to #.
+In the first while loop of (41-45) we use the substring i,j to grab the encoded length of the string (this is the value before the #).
+Then from 46-48, we are moving the pointers to the actual string (i being 1 position after the #, and J being the length of the string after the #).
+
+
+If the encoded was "5#Hello5#World"
+
+DECODING "5#Hello5#World":
+1. First iteration:
+Find '#' after '5'
+Extract length = 5
+Extract string from next 5 chars: "Hello"
+ Push "Hello" to result
+
+2. Second iteration:
+Find '#' after '5'
+Extract length = 5
+Extract string from next 5 chars: "World"
+Push "World" to result
+
+*/
     
