61a2f97c8e5e511ba1d9de03ea4a8e037e2811f3
Victor <ban.victor728@gmail.com>
2024-11-01 13:19:34 -0400

    added topkfrequent sort solution variant

--- 1_arrayshashing/3_2topKFrequentElements.js
+++ 1_arrayshashing/3_2topKFrequentElements.js
@@ -21,7 +21,25 @@ It is guaranteed that the answer is unique.
 Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
 */
 
-// Sorting
+// Sorting - hashmap
+
+var topKFrequentSort = function (nums, k) {
+  const count = {};
+  for (const num of nums) { // store the value as [number/frequency]
+      count[num] = (count[num] || 0) + 1; 
+  } 
+
+  const arr = Object.entries(count).map(([num, freq]) => [freq, parseInt(num)]);   // change the map from [number/frequency it shows up] to [frequency/number] 
+  arr.sort((a, b) => b[0] - a[0]);   // and then sort from most frequent to least frequent
+
+  return arr.slice(0, k).map(pair => pair[1]); // return a sliced array from the most frequent to the desired times K
+}
+
+/* I think the easy part is knowing yes we know to use a map to build the relationship between value & counts.
+The tricky thing here is realizing we can turn the map into an array & then flip the map key value pair to "value-key" for the sort (line 32).
+
+Sorting causes the logN addition to the time complexity
+*/
 
 // Heap 
 
