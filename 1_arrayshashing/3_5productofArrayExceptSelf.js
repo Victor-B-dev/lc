70c7d98d35df966434ae5f518ad9d03cee21131b
Victor <ban.victor728@gmail.com>
2024-11-06 19:23:06 -0500

    added division solution with zeros edge cases

--- 1_arrayshashing/3_5productofArrayExceptSelf.js
+++ 1_arrayshashing/3_5productofArrayExceptSelf.js
@@ -39,17 +39,17 @@ const productExceptSelfBF = (nums) => {
 
 // Full double loop means O(n^2). Can easily do better.
 
-// 2. Division
+// 2.a Division
 const productExceptSelfDivSimple = (nums) => {
   const result = [];
   let product = 1;
 
   for (let i = 0; i < nums.length; i++){
-    product *= nums[i];
+    product *= nums[i]; // find the product of all the numbers
   }
 
   for (let i = 0; i < nums.length; i++){
-    result.push(product / nums[i]);
+    result.push(product / nums[i]); // divide the product by the nums and store each result in the result array
   }
 
   return result
@@ -60,6 +60,45 @@ We know each total product is multiplying all numbers so dividing by each number
 Redundant work.
 
 I want to note that there is an obvious mathematical edge case here that should be accounted for but I want to show that as a separate solution.
+
+This case is obviously being if a number in the array is a 0 which would make the product 0 and then later dividing 0 by 0 is going to throw you an error.
+*/
+
+// 2.b Division - Zero Count Edge Case
+
+const productExceptSelfDivZeroCount = (nums) => {
+  const result = [];
+  let product = 1;
+  let zeroCount = 0;
+
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] !== 0){
+      product *= nums[i];
+    } else {
+      zeroCount++;
+    }
+  }
+
+  if (zeroCount > 1) {
+    return Array(nums.length).fill(0); // if there is more than one zero, all the numbers will be 0
+  }
+
+  for (let i = 0; i < nums.length; i++){
+    if (zeroCount === 1) { // if there is exactly one zero,
+      result[i] = (nums[i] === 0) ? product: 0; // at that index, it will not be included in the product, so we still need that result, every other position of the result array will otherwise be 0
+    } else {
+      result.push(product / nums[i]); // if there are no zeros, we simply divide the product by 0
+    }
+  }
+
+  return result
+}
+
+/* Remember the original product is the product of the array except a given index in an array.
+
+If there is more than one zero, everything will be zero.
+If there is one zero, it won't be included so you still need the product of everything else.
+Because those edge cases break the function completely, the logic flow is as shown above so we don't do any work we don't need to.
 */
 
 // 3. Prefix Suffix (Optimized)
