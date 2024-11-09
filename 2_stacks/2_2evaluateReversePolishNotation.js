d7c19d01ba9817d82936a3af48e8d2bd13a308b7
Victor <ban.victor728@gmail.com>
2024-11-08 19:21:39 -0500

    added eRPN solution

--- 2_stacks/2_2evaluateReversePolishNotation.js
+++ 2_stacks/2_2evaluateReversePolishNotation.js
@@ -37,4 +37,35 @@ Constraints:
 tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
 */
 
-// Solution
+// Solution - stack
+
+var evalRPN = function(tokens) {
+  const stack = [];
+
+      for (const token of tokens) {
+          if (token === '+') {
+              stack.push(stack.pop() + stack.pop());
+          } else if (token === '-') {
+              const a = stack.pop();
+              const b = stack.pop();
+              stack.push(b - a);
+          } else if (token === '*') {
+              stack.push(stack.pop() * stack.pop());
+          } else if (token === '/') {
+              const a = stack.pop();
+              const b = stack.pop();
+              stack.push(Math.trunc(b / a));
+          } else {
+              stack.push(parseInt(token));
+          }
+      }
+  return stack.pop();
+};
+
+/* Biggest problem is just understanding the question. Read the examples, plan out steps of how to parse the solution.
+
+The most likely problem people would find I believe is realizing to push a mathematic expression back onto the stack for use.
+But otherwise once you know about that trick of storing the solution on top of the stack, it becomes easier to see how the code works.
+
+The if/elses are a bit ugly and a good exercise here would be to use switch case for clarity.
+*/
