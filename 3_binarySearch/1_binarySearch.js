968f828af90bef0cb9f9a880ee94a7853e86997c
Victor <ban.victor728@gmail.com>
2024-11-18 13:17:42 -0500

    added recursive binarysearch solution

--- 3_binarySearch/1_binarySearch.js
+++ 3_binarySearch/1_binarySearch.js
@@ -22,4 +22,30 @@ All the integers in nums are unique.
 nums is sorted in ascending order.
 */
 
-// Solution
+/* Binary search comes in variety of flavors & is an extremely common problem. It runs in logn time (we keep dividing successively inputs by 2), which compared to usual brute force searches that are likely On^2, it's a massive time scaling save.
+
+This file will have more solutions than necessary to introduce us to new concepts.
+
+Binary searches usually rely on the input array being sorted because part of the basic premise of the search is to quickly and efficiently eliminating needless computations based on that assumption.
+
+As such future problems will rely on us learning about sorting.
+*/
+
+// Solution - Recursive BFS
+
+var searchRecursive = function (nums, target) {
+
+  var binarySearch = function (left, right, nums, target) { // helper function
+    if (left > right ) return -1;
+    let middle = Math.floor ((left + right) / 2); // go to the middle
+
+    if (nums[middle] === target) return middle; // if the middle is the target, escape 
+    if (nums[middle] < target) { // if the middle number is less than the target
+      return binarySearch(middle + 1, right, nums, target) // recursiveley search to the right
+    } else {
+      return binarySearch(left, middle - 1, nums, target) // otherwise search to the left, you could also write the converse of this situation
+    }
+  }
+
+  return binarySearch(0, nums.length - 1, nums, target) // call helper function on input
+}
