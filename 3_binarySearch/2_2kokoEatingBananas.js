9241d5d366169582bb94cd4de823f642e5cbee64
Victor <ban.victor728@gmail.com>
2024-11-21 01:58:49 -0500

    added binary search solution for koko eating bananas

--- 3_binarySearch/2_2kokoEatingBananas.js
+++ 3_binarySearch/2_2kokoEatingBananas.js
@@ -32,5 +32,36 @@ piles.length <= h <= 109
 Solve for the minimum speed (bananas per hour) that koko needs to eat if we are given "h" the amount of time she has to eat.
 We are also given different piles of different bananas #s.
 We are also given the fact that she does not start another pile immediately when she finishes a pile. 
+
+Other information we can infer is 2 things.
+The minimum time it can take is piles.length because coco only eats at most one pile per hour.
+Therefore the max rate for "k" that we care about is the pile with the max number of bananas in it.
+For example 1, [3,6,7,11] that would be 11. 
+The arbitrary minimum would be 1. So K would be value between [1...11].
+We can use binary search on this range to more quickly solve the problem instead of brute forcing every solution.
+
+Figuring out the bounds for the binary search is the tricky part
 */
+
+var minEatingSpeed = function (piles, h){
+  let left = 1;
+  let right = Math.max(...piles);
+  let result = right; // we set result to the right because it's the maximum number of bananas
+
+  while (left <= right){
+    const k = Math.floor((left + right) / 2); // we start the rate in the middle
+    
+    let totalTime = 0;
+    for (const pile of piles){
+      totalTime += Math.ceil (pile / k); // math.ceil rounds up to the nearest whole integer because coco always uses the whole hour
+    }
+    if (totalTime <= h) { // if the total time is less than or equal to the minimum hours
+      result = k;
+      right = k - 1;
+    } else {
+      left = k + 1;
+    }
+  }
+  return result;
+}
 
