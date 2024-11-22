c819c287280badb76c5410d0ade45a1b848accb2
Victor <ban.victor728@gmail.com>
2024-11-22 16:50:06 -0500

    added one pass binary search solution to rotatedsortedarray

--- 3_binarySearch/2_4searchinRotatedSortedArray.js
+++ 3_binarySearch/2_4searchinRotatedSortedArray.js
@@ -84,3 +84,34 @@ var searchBinarySearch = function (nums, target){
 
   return -1 // if not found return -1
 }
+
+// Binary Search - One Pass
+
+var searchBSOP = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (target === nums[mid]) { // early return
+      return mid;
+    }
+
+    if (nums[left] <= nums[mid]) {
+      if (target > nums[mid] || target < nums[left]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+        }
+    } else {
+      if (target < nums[mid] || target > nums[r]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+
+  return -1;
+}
