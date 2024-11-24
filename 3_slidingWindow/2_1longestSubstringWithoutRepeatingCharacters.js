c26f07455165f8703a2fa28b43e1841ec5aa7009
Victor <ban.victor728@gmail.com>
2024-11-24 02:11:33 -0500

    Added brute force for longest substring without repeating hcars

--- 3_slidingWindow/2_1longestSubstringWithoutRepeatingCharacters.js
+++ 3_slidingWindow/2_1longestSubstringWithoutRepeatingCharacters.js
@@ -18,3 +18,21 @@ Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
+// Brute Force - Set used
+
+var lengthOfLongestSubstringBF = function (s){
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++){ // for each character in the string
+    let charSet = newSet(); // initialize a set;
+    for (let j = i; j <s.length; j++){ // initialize a second pointer to iterate through the string
+      if (charSet.has(s[j])){  // check if the current character exists in the set
+        break; // break the current loop
+      }
+      charSet.add(s[j]); // otherwise add the character to the set
+    }
+    result = Math.max(result, charSet.size); // compare the result and the current set's length/size, keep the larger one 
+  }
+  return result // return 
+}
