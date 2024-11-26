5b650d4e0a92926f6100ae9ee168382fd2a7889d
Victor <ban.victor728@gmail.com>
2024-11-26 00:45:21 -0500

    added tortoise hari solution to cycle linked list problem

--- 3_linkedLists/1_3linkedListCycle.js
+++ 3_linkedLists/1_3linkedListCycle.js
@@ -31,7 +31,7 @@ Follow up: Can you solve it using O(1) (i.e. constant) memory?
 
 // Hashset - Non Optimal
 
-var hasCycle = function (head){
+var hasCycle = function(head){
   let visited = new Set(); // create a set - a structure that doesn't allow duplicates - fast O(1) retrieval
   let current = head;
 
@@ -47,7 +47,35 @@ var hasCycle = function (head){
 }
 
 /* As suggested in the constraint/followup, a hashset is a naive solution because it requires additional memory to compute.
+
 There is a better way that doesn't involve extra memory and likely you've already encountered it.
+It comes in a lot of of names -- and logically you can probably come up with it yourself.
 */
 
-// Fast/Slow Pointers -Tortoise and Hare Algorithm - Floyud's Cycle Finding Algorithm
+// Fast/Slow Pointers - Tortoise and Hare Algorithm - Floyd's Cycle Finding Algorithm
+
+var hasCycleTH = function(head){
+  let fast = head;
+  let slow = head;
+
+  while (fast !== null && fast.next !== null){
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/* The basic premise of the algorithm is basically that the fast pointer will eventually catch up to the slow pointer if there's a cycle. 
+It doesn't matter how many steps there are between the fast and the slow, because so long as the fast is moving faster than the slow, step by step, it will reach the slow pointer since the presence of a cycle means the slow one will go on indefinitely.
+
+If there isn't a cycle -- we don't care when the slow reaches the end, we only care about the fast pointer since that will dictate the cycle existence.
+
+If the fast pointer reaches a null, then there's no cycle. But if it doesn't reach a null && it reaches the slow pointer, then there's a cycle. (We don't want the fast pointer to early return if it reaches the slow early either.)
+
+Hence the while loop that it runs until it && its next reaches a null.
+*/
