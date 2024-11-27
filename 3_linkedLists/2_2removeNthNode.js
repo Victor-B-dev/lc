13a4db9995544f5dceda8febbb1a3b82887f0f12
Victor <ban.victor728@gmail.com>
2024-11-26 23:59:04 -0500

    Added brute force solution

--- 3_linkedLists/2_2removeNthNode.js
+++ 3_linkedLists/2_2removeNthNode.js
@@ -21,5 +21,24 @@ The number of nodes in the list is sz.
 0 <= Node.val <= 100
 1 <= n <= sz
 
-
 Follow up: Could you do this in one pass? */
+
+// Brute Force - Changing it into an array to be able to get to the correct
+
+var removeNthFromEnd = function(head, n){
+  const nodes =[];
+  let current = head;
+  while (current){
+    nodes.push(current)
+    current= current.next;
+  }
+
+  const removeIndex = nodes.length - n;
+  if (removeIndex === 0){  // if we're removing the first node
+    return head.next; // we return from the second node
+  }
+
+  nodes[removeIndex - 1].next = nodes[removeIndex].next; // otherwise we set the previous node's next to the removed node's next (circumventing it)
+  return head;
+}
+
