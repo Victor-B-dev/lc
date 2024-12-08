3c860f2daa0bf41069777457210d97892c7103c3
Victor <ban.victor728@gmail.com>
2024-12-07 22:12:21 -0500

    added python hashmap answer for two sums problem

--- 1_arrayshashing/1_twoSum.py
+++ 1_arrayshashing/1_twoSum.py
@@ -1,6 +1,17 @@
 def twoSumBF (nums):
     for i in range(len(nums)):
-        for j in range (i + 1, len(nums)):
+        for j in range(i + 1, len(nums)):
             if (nums[i] + nums[j] == target):
                 return [i,j]
+    return []
+
+
+def twoSumHM(nums,target):
+    num_map = {}
+    
+    for i, num in enumerate(nums):
+        difference = target - num
+        if difference in num_map:
+            return [num_map[difference], i];
+        num_map[num] = i
     return []
