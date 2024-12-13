2637951091600b92d1d6bd7194f82632d16e5bdd
Victor <ban.victor728@gmail.com>
2024-12-12 19:11:37 -0500

    added containsDuplicate sort solution

--- 1_arrayshashing/2_2containsDuplicate.py
+++ 1_arrayshashing/2_2containsDuplicate.py
@@ -5,3 +5,10 @@ def hasDuplicateBF(nums) -> bool:
                 return True
         return False
     
+def hasDuplicateSorting(nums) -> bool:
+    nums.sort()
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return True
+    return False
+
