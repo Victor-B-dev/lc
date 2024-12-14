2995d540f704aff890f026deda11a4a481a1705c
Victor <ban.victor728@gmail.com>
2024-12-14 11:24:22 -0500

    added group anagram hash table solution

--- 1_arrayshashing/3_2groupAnagrams.py
+++ 1_arrayshashing/3_2groupAnagrams.py
@@ -7,3 +7,15 @@ def groupAnagramSort(strs):
         res[sorted_word].append(word)
 
     return list(res.values())
+
+
+def groupAnagramsHS(strs):
+    res = defaultdict(list)
+
+    for word in strs:
+        count = [0] * 26
+        for char in word:
+            count[ord(char) - ord('a')] +=1
+        res[tuple(count)].append(word)
+    
+    return list(res.values())
