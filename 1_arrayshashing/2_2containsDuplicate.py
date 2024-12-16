0d75f1c23e11a53f50904b5d29a115d15cc3044d
Victor <ban.victor728@gmail.com>
2024-12-16 00:09:30 -0500

    changed containsDupklicate.py to OOP syntax

--- 1_arrayshashing/2_2containsDuplicate.py
+++ 1_arrayshashing/2_2containsDuplicate.py
@@ -1,22 +1,26 @@
-def hasDuplicateBF(nums) -> bool:
-    for i in range(len(nums)):
-        for j in range (i + 1, len(nums)):
-            if nums[i] == nums[j]:
+from typing import List
+
+class SolutionBF:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range (i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+class SolutionSort:    
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
                 return True
         return False
-    
-def hasDuplicateSorting(nums) -> bool:
-    nums.sort()
-    for i in range(1, len(nums)):
-        if nums[i] == nums[i - 1]:
-            return True
-    return False
 
-def hasDuplicateHS(nums) -> bool:
-    hashset = set()
-    for num in nums:
-        if num in hashset:
-            return True
-        hashset.add(num)
-    return False
-
+class solutionHashMap:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for num in nums:
+            if num in hashset:
+                return True
+            hashset.add(num)
+        return False
