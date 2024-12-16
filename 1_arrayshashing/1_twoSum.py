9110048f3de0b37d887e2302ff649111424f9164
Victor <ban.victor728@gmail.com>
2024-12-16 00:05:55 -0500

    changed syntax for twosum.py to use OOP

--- 1_arrayshashing/1_twoSum.py
+++ 1_arrayshashing/1_twoSum.py
@@ -1,17 +1,21 @@
-def twoSumBF (nums):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if (nums[i] + nums[j] == target):
-                return [i,j]
-    return []
+from typing import List
+
+class SolutionBF:
+    def twoSum (self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    return [i,j]
+        return []
 
 
-def twoSumHM(nums,target):
-    num_map = {}
+class SolutionHashmap:
+    def twoSum (self, nums: List[int], target: int) -> List[int]:
+        num_map = {}
     
-    for i, num in enumerate(nums):
-        difference = target - num
-        if difference in num_map:
-            return [num_map[difference], i];
-        num_map[num] = i
-    return []
+        for i, num in enumerate(nums):
+            difference = target - num
+            if difference in num_map:
+                return [num_map[difference], i];
+            num_map[num] = i
+        return []
