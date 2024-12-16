9b8d35527503bcef4a9b92c629739c027be3c22f
Victor <ban.victor728@gmail.com>
2024-12-16 00:39:02 -0500

    added bucket sort solution for topkfrequentelements.py

--- 1_arrayshashing/3_2topKFrequentElements.py
+++ 1_arrayshashing/3_2topKFrequentElements.py
@@ -20,3 +20,17 @@ class SolutionSort:
 
 class SolutionBucketSortHashmap:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, num_count in count.items():
+            freq[num_count].append(num)
+        
+        result = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
