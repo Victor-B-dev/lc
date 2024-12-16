bd088242d3e2b2b5186256d9d49e86e228cf64d5
Victor <ban.victor728@gmail.com>
2024-12-16 00:17:58 -0500

    changed validAnagram.py into OOP syntax

--- 1_arrayshashing/2_1validAnagram.py
+++ 1_arrayshashing/2_1validAnagram.py
@@ -1,16 +1,18 @@
-def isAnagramSort(s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
-    
-    return sorted(s) == sorted(t)
+class SolutionSort:
+    def isAnagram(s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
 
-def isAnagramHT(s: str, t:str) -> bool:
-    if len(s) != len(t):
-        return False
-    
-    countS, countT = {}, {}
 
-    for i in range(len(s)):
-        countS(s[i]) = 1 + countS.get(s[i],0)
-        countT(t[i]) = 1 + countS.get(s[i],0)
-    return countS == countT
+class SolutionHashTable:
+    def isAnagramHT(s: str, t:str) -> bool:
+        if len(s) != len(t):
+            return False
+    
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS(s[i]) = 1 + countS.get(s[i],0)
+            countT(t[i]) = 1 + countS.get(s[i],0)
+        return countS == countT
