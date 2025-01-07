fc132b73c2916dc75377a4c5e0603f83e9a609bd
Victor <ban.victor728@gmail.com>
2025-01-07 14:39:17 -0500

    added typing support for linkedlist.py, will need tod o this in the future

--- 3_linkedLists/3_reversedLinkedList.py
+++ 3_linkedLists/3_reversedLinkedList.py
@@ -1,7 +1,14 @@
-def reverse_list(head):
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_list(self, head: ListNode) -> ListNode:
     prev = None
     current = head
-
+    
     while current:
         temp = current.next
         current.next = prev
