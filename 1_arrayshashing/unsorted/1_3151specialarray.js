4200cd5e949415335fd8ac001da59adc3f1effdf
Victor <ban.victor728@gmail.com>
2025-02-01 11:35:31 -0500

    added solution for isArraySpecial

--- 1_arrayshashing/unsorted/1_3151specialarray.js
+++ 1_arrayshashing/unsorted/1_3151specialarray.js
@@ -25,3 +25,27 @@ Explanation:
 
 nums[1] and nums[2] are both odd. So the answer is false.
 */
+
+/* The only confusing part of this is figuring out what parity means.
+In this case, it's asking if adjacent values are both odd or even.
+
+Return true if they are never both odd/equal.
+Return false (early exit) if they are.
+
+With that knowledge, solution is simple, we just modulus the the values and check if they're equal.
+*/
+
+var isArraySpecial = function(nums) {
+  for (let i = 1; i <= nums.length; i++){ // starting at first index
+      let curr = nums[i] // current index
+      let prev = nums[i - 1] // previous index
+
+
+      if ((prev % 2 === 0) && (curr % 2 === 0) // if both even
+          || (prev % 2 === 1) && (curr % 2 === 1)){ // or both odd
+          return false // early exit
+      }
+  }
+
+  return true; // if all pass, it's special
+};
