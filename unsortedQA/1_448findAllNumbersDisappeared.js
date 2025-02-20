c83cf64b9732530d6b7f00af71d268842625c7f2
Victor <ban.victor728@gmail.com>
2025-02-20 12:24:17 -0500

    added optimal solution for lc # 448

--- unsortedQA/1_448findAllNumbersDisappeared.js
+++ unsortedQA/1_448findAllNumbersDisappeared.js
@@ -39,3 +39,37 @@ var findDisappearedNumbers = function(nums) {
 /* Two little tricks here.
 First is that we are told to initialize at 1; so for the loop we need i <= nums.length.
 Second is what we are comparing against the hashset is the index counter that's why line 31-32 has only "i".*/
+
+
+// Optimal Solution - O(2n) time becomes O(n) time, o(1) space
+
+var findDisappearedNumbersOptimal = function(nums) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++){
+    const num = Math.abs(num[i]) - 1;
+    if (nums[num] > 0){
+      nums[num] = -nums[num];
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++){
+    if (nums[i] > 0){
+      result.push(i + 1);
+    }
+  }
+
+  return result
+};
+
+/* The way to not use extra space is to mutate the original array.
+We convert all the numbers in the array into a negative value to consider it as seen.
+Any remaining index is a missing number.
+
+To note similar to missing number (lc # 268), one needs to realize that the index counter is part of the solution in there being a contiguous set of numbers vs the missing numbers.
+In such that the index is the comparison hence lines 49-52 is how we mark the values that have been seen.
+
+So the data we are mainpulating is values to indexes, then indexes back to value.
+*/
+
+
