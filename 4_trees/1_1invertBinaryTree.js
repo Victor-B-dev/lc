30fe0a533f367a1c6eef88aa12d9bb5f193632b5
Victor <ban.victor728@gmail.com>
2025-02-28 22:35:29 -0500

    added iterative dfs solution

--- 4_trees/1_1invertBinaryTree.js
+++ 4_trees/1_1invertBinaryTree.js
@@ -43,3 +43,22 @@ var invertTreeDFS = function(root) {
 
   return root;
 };
+
+// DFS solution - iterative
+
+var invertTreeDFS2 = function(root) {
+  if (!root) return null;
+
+  const stack = [root];
+  while (stack.length) {
+    const node = stack.pop();
+
+    [node.left, node.right] = [node.right, node.left];
+    
+    if (node.left) stack.push(node.left);
+    if (node.right) stack.push(node.right);
+  }
+  
+  return root;
+};
+
